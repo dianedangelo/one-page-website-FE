bcd7ef2cb71826ae6dc6161a37b31fa7bcdba2b7
Diane D'Angelo <ddangelo@nyc.rr.com>
2017-01-31 15:58:26 -0500

    new things added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,5 +18,42 @@
 
 		$(".fancybox").fancybox();
 
+///////////////////////////////////////////
+
+
+
+// }); $(window).on("scroll",function(){
+ // 	if(window.scrollY < 320){
+ // 		// $("#socialMedia").css("position","absolute");
+ // 	$("#socialMedia").removeClass("sticky");
+ // 	} else {
+ // 		// $("#socialMedia").css("position", "fixed");
+ // 		$("#socialMedia").addClass("sticky");
+ // 	}
+ // 	if(window.scrollY < 603){
+ // 		$("header").css("background-color","rgba(100,10,20,.75)");
+ // 	}
+ // 	else{
+ // 		$("header").css("background-color","rgba(200,0,0,.5)" );
+ // 	}
+ // });
+
+	// $(".link").on("click",function(e){
+	// 	e.preventDefault();
+	// 	var scrollTime = 500;
+	// 	if($(this).hasClass("link_home")){
+	// 	$.scrollTo("#home",scrollTime, {
+	// 	offset: -100
+	// 	});
+	// 	}else if($(this).hasClass("link_work")){
+	// 	$.scrollTo("#work", scrollTime);
+	
+	// 	}else if($(this).hasClass("link_resume")){
+	// 	$.scrollTo("#resume", scrollTime);
+
+	// 	}
+	// });
 
 });
+
+
